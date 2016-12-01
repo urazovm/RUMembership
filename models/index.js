973c906ace538996fa95626c6e075ab40dfe7234
mayjest <mayjest@gmail.com>
2016-12-01 21:33:23 +0000

    Removed pg heroku path from comment

--- models/index.js
+++ models/index.js
@@ -4,8 +4,6 @@ if (!global.hasOwnProperty('db')) {
     var Sequelize = require('sequelize');
     var sequelize = null;
 
-
-    //postgres://jhjuwubwlcfpcn:3yCcNJh33wJTPHMwBMyQ5ug5Qp@ec2-54-235-102-235.compute-1.amazonaws.com:5432/ddhvk755r9963m
     var dbConnectionString = process.env.DATABASE_URL;
     if (!process.env.DATABASE_URL) {
         dbConnectionString = "postgres://postgres:Password1@localhost:5432/ruDev";
