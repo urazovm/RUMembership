d770eaf994aa007a549a828393ba31496ae3d99f
mayjest <mayjest@gmail.com>
2016-12-08 22:01:02 +0000

    Better variable check

--- controllers/utils.js
+++ controllers/utils.js
@@ -1,5 +1,5 @@
-exports.requiredVariable = function(variable, varName, missing) {
-  if (!variable) {
+exports.requiredVariable = function (variable, varName, missing) {
+  if (typeof variable == 'undefined') {
     missing.push(varName);
   }
   return missing;
