4c9e33d9c17364f9cb26a33a3599922c6aabd99d
Mayjest <mayjest@gmail.com>
2016-12-22 17:17:23 +0000

    Rejigged routes a bit for heroku

--- server/app.js
+++ server/app.js
@@ -22,10 +22,14 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use('/api', routes);
 
+if (app.get('env') === 'production') {
+  console.log(path.join(__dirname, '../dist/client', 'index.html'));
+  app.use(express.static(path.join(__dirname, '../dist/client', 'index.html')));
+}
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   var err = new Error('Not Found');
--- server/routes/index.js
+++ server/routes/index.js
@@ -4,18 +4,21 @@ var router = express.Router();
 
 var api = require('./api');
 
-router.use('/api', api);
+router.use('/', api);
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  console.log('in API');
-  res.json({ "apiRoot": true });
-});
+// /* GET home page. */
+// router.get('/', function (req, res, next) {
+//   console.log('in API');
+//   res.json({ "apiRoot": true });
+// });
 
-// Once we start with a client and have authentication, 
-// this will redirect to the angular app
-router.get("/*",  function(req, res, next) {
-  res.sendFile(path.join(__dirname, '../', 'index.html'));
-});
+// // Once we start with a client and have authentication, 
+// // this will redirect to the angular app
+// router.get("/*", function (req, res, next) {
+//   console.log('redirect to angular');
+//   console.log(__dirname);
+//   console.log(path.join(__dirname, '../dist/client', 'index.html'));
+//   res.sendFile(path.join(__dirname, '../dist/client', 'index.html'));
+// });
 
 module.exports = router;
